3f81c1af94948a03c79be26eb3a27203f36b5988
Jeffrey Rios Muñoz <jeffrey.rios@siesa.com>
2020-03-20 13:26:15 -0500

    ejemplo de sp agregado

--- backend/src/database/SP/EjemploSP.sql
+++ backend/src/database/SP/EjemploSP.sql
@@ -1,3 +1,14 @@
+CREATE DATABASE postgres_node_api;
+
+USE postgres_node_api;
+
+CREATE TABLE users(
+  id SERIAL PRIMARY KEY,
+  nombre VARCHAR(250),
+  clave VARCHAR(250),
+  rol VARCHAR(250)
+);
+
 CREATE OR REPLACE FUNCTION f_get_users()
 RETURNS SETOF users AS
 $BODY$
@@ -16,3 +27,14 @@ BEGIN
     RAISE WARNING 'The stack trace of the error is: "%"', v_error_stack;
 END
 $BODY$ LANGUAGE 'plpgsql'
+
+
+insert into users
+(nombre, clave, rol)
+values(
+	'jeffrey',
+	md5('123'),
+	'admin'
+);
+
+select * from f_get_users()
