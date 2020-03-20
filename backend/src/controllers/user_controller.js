736a409eec1d397500aad1c72f404d056b2868cd
Jeffrey Rios Muñoz <jeffrey.rios@siesa.com>
2020-03-20 13:25:03 -0500

    ejemplo de como ejecutar funcion de bd desde nodejs terminado

--- backend/src/controllers/user_controller.js
+++ backend/src/controllers/user_controller.js
@@ -5,16 +5,17 @@ const rscController = require('../resources/rsc_controller')
 userController.getUsers = async (req, res) => {
     try {
         let sql = "select * from f_get_users()";
-        await connection.query(sql, (err, results)=>{
-            if(!err){
-                res.status(200).json(results.rows)
-            }else{
-                res.status(204).json({ err: err});
+        await connection.query(sql, (err, results) => {
+            if (!err) {
+                res.status(200).json(results.rows);
+
+            } else {
+                res.status(204).json({ err: err });
             }
-        });       
+        });
     } catch (error) {
-        res.status(204).json({ err: error});
-        // res.status(204).send(rscController.leerRecurso(1000))
+        res.status(204).json({ err: error });
+        // res.status(200).json(rscController.leerRecurso(1000));
     }
 }
 
