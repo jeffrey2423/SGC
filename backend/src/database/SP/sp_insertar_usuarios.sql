fb63d894c99b342c625bf621374c62530c832f55
Jeffrey Rios Muñoz <jeffrey.rios@siesa.com>
2020-03-20 21:20:30 -0500

    ejemplo de funcion con parametro json terminada

--- backend/src/database/SP/sp_insertar_usuarios.sql
+++ backend/src/database/SP/sp_insertar_usuarios.sql
@@ -1,19 +1,44 @@
-CREATE FUNCTION insert_from_json(in_json_txt json) RETURNS void
-    LANGUAGE plpgsql
+CREATE OR REPLACE FUNCTION insert_from_json(in_json_txt json) RETURNS void
     AS 
-    $BODY$
+$BODY$
 DECLARE
-    reg RECORD;
     v_error_stack text;
 
-BEGIN
-    INSERT INTO
-        table_name (name, age)
-    SELECT
-        (rec ->> 'name') :: text,
-        (rec ->> 'age') :: integer
-    FROM
-        json_array_elements(in_json_txt -> 'data') rec
+    BEGIN
+        CREATE TABLE IF NOT EXISTS t_temp_json_data(
+            id          SERIAL NOT NULL PRIMARY KEY,
+            extra_info  JSON
+        );
 
+        INSERT INTO t_temp_json_data(extra_info)
+        VALUES(
+           in_json_txt 
+        );
+
+    INSERT INTO users(
+        nombre,
+        clave,
+        rol
+    )SELECT
+        extra_info->>'nombre' AS nombre,
+        md5(extra_info->>'clave') AS clave,
+        extra_info->>'rol' AS rol
+    FROM t_temp_json_data;
+
+	EXCEPTION WHEN OTHERS THEN
+	GET STACKED DIAGNOSTICS v_error_stack = PG_EXCEPTION_CONTEXT;
+
+    RAISE WARNING 'The stack trace of the error is: "%"', v_error_stack;
+
+    DROP TABLE t_temp_json_data;
 END 
-$ BODY $ LANGUAGE 'plpgsql'
+$BODY$ LANGUAGE 'plpgsql'
+
+
+
+/*
+ASI SE LLAMA
+
+SELECT insert_from_json('{"nombre":"jeffreyPRUEBA","clave":"5555","rol":"rollPRUEBA"}')
+
+select * from users*/
