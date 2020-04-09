019d1492a61c676a0e5472d9faa9f1604802882f
Jeffrey Rios Muñoz <jeffrey.rios@siesa.com>
2020-04-09 17:26:30 -0500

    mejora en la funcion para insertar un usuario y se agregan nuevas funciones

--- backend/src/database/SP/f_insertar_usuario.sql
+++ backend/src/database/SP/f_insertar_usuario.sql
@@ -1,26 +1,37 @@
-CREATE OR REPLACE FUNCTION f_insertar_usuario(in_json_txt json) RETURNS void
+CREATE OR REPLACE FUNCTION f_insertar_usuario(in_json_txt JSON) RETURNS void
     AS
 $BODY$
 	DECLARE
 		err_context text;
 
     BEGIN
-		WITH json_data(datajson) as 
-		(
-			values(
-				in_json_txt
-			)
-		)
 
-    INSERT INTO users(
+    INSERT INTO t1004_usuarios(
+        f1004_nombre,
+        f1004_apellido,
+        f1004_fecha_nacimiento,
+        f1004_id_profesion_t1003,
+        f1004_email,
+        f1004_clave,
+        f1004_id_perfil_t1000
+    )
+    SELECT
         nombre,
-        clave,
-        rol
-    )SELECT
-        datajson->>'nombre' AS nombre,
-        md5(datajson->>'clave') AS clave,
-        datajson->>'rol' AS rol
-    FROM json_data;
+        apellido,
+        fecha_nacimiento,
+        id_profesion,
+        email,
+        MD5(clave),
+        id_perfil
+    FROM json_to_record(in_json_txt) AS x( 
+        nombre text,
+        apellido text,
+        fecha_nacimiento date,
+        id_profesion int,
+        email text,
+        clave text,
+        id_perfil int
+    );
 
 EXCEPTION WHEN OTHERS THEN
         GET STACKED DIAGNOSTICS err_context = PG_EXCEPTION_CONTEXT;
@@ -30,6 +41,3 @@ EXCEPTION WHEN OTHERS THEN
 
 END
 $BODY$ LANGUAGE 'plpgsql'
-
-/*select f_insert_user_from_json('{"nombre":"jeff","clave":"5555","rol":"jeff"}');
-select * from users;*/
