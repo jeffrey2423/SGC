3bf7f02d91b1ac995c4cde80a873d31d955fee4b
jpcv222 <juan.castro.vasquez@correounivalle.edu.co>
2020-04-14 02:45:05 -0500

    Add image logo on header

--- sgc_client/src/App.js
+++ sgc_client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 
+import Header from './/components/header'
 import Menu from './/components/NavBar'
 import GestionPerfiles from './/components/GestionPerfiles'
 
@@ -10,6 +11,7 @@ import './App.css'
 function App() {
   return (
     <Router>
+      <Header />
       <Menu />
       <div className="container p-4">
         <Route path="/GestionPerfiles" exact component={GestionPerfiles} />
--- sgc_client/src/components/header.js
+++ sgc_client/src/components/header.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
 
-export default class NavBar extends Component {
+export default class header extends Component {
     render (){
+        return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 <Link className="header-logo" to="/Calendar">
-                    ValentinSpa
+                    <img src ="./spa-logo.PNG" title="ValentinSpa" className="img-logo"/>
                 </Link>
         </nav>
+        )
     }
 }
