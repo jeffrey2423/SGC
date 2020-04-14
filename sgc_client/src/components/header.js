69ad696384fafd835a9aa7ce1bbcdd23bc8a0bb8
jpcv222 <juan.castro.vasquez@correounivalle.edu.co>
2020-04-14 02:50:21 -0500

    Add header styles

--- sgc_client/src/components/header.js
+++ sgc_client/src/components/header.js
@@ -3,8 +3,15 @@ import { Link } from 'react-router-dom';
 
 export default class header extends Component {
     render (){
+        const style = {
+            margin: '0.5em',
+            paddingLeft: 0,
+            listStyle: 'none',
+            background: '#612080'
+          };
+
         return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <nav className="navbar navbar-expand-lg" style={style}>
                 <Link className="header-logo" to="/Calendar">
                     <img src ="./spa-logo.PNG" title="ValentinSpa" className="img-logo"/>
                 </Link>
