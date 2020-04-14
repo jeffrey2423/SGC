bf29c65ec1da1a3e9dbb86b644d065367c890d4e
jpcv222 <juan.castro.vasquez@correounivalle.edu.co>
2020-04-14 03:37:27 -0500

    Add GestionCitas component

--- sgc_client/src/components/GestionCitas.js
+++ sgc_client/src/components/GestionCitas.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
 
-export default class Inicio extends Component {
+export default class GestionCitas extends Component {
     render() {
         return (
             <div>
-                Esto es inicio
+                
             </div>
         )
     }
