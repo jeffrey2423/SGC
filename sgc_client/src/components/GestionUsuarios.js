350966621563f23bfb41d82b0a08d21b338c52c6
jpcv222 <juan.castro.vasquez@correounivalle.edu.co>
2020-04-15 02:48:41 -0500

    Update selector mouse events to users table

--- sgc_client/src/components/GestionUsuarios.js
+++ sgc_client/src/components/GestionUsuarios.js
@@ -24,7 +24,7 @@ export default class GestionUsuarios extends Component {
 
     alertHoverTable = () => {
         $(document).ready(function(){
-            $('tr').not(':first').mouseover(function(){
+            $('div.tableuser tr').not(':first').mouseover(function(){
                 var user = $(this).find('td:first-child').text();
                 $("#message-mouseover").empty();
                 $("#message-mouseover").append("Click sobre la fila para actualizar");  
