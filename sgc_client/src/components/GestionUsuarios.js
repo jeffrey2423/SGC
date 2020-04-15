901550aeeb75200fda60d7ac147c763dade50749
jpcv222 <juan.castro.vasquez@correounivalle.edu.co>
2020-04-15 01:40:44 -0500

    Add users datatable

--- sgc_client/src/components/GestionUsuarios.js
+++ sgc_client/src/components/GestionUsuarios.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
+import UserDatatablePage from './UserDatatablePage'
 
 export default class GestionUsuarios extends Component {
     render() {
         return (
-            <div>
-                Esto es gestión de usuarios
+            <div className="container">
+               <h2>Usuarios de la aplicación</h2>
+                <hr />
+                <UserDatatablePage />
             </div>
         )
     }
