687c5ca4777cd197384f1a2ac0adc430c524d905
jpcv222 <juan.castro.vasquez@correounivalle.edu.co>
2020-04-15 02:48:02 -0500

    Add mouse events to users table

--- sgc_client/src/components/GestionUsuarios.js
+++ sgc_client/src/components/GestionUsuarios.js
@@ -6,28 +6,53 @@ export default class GestionUsuarios extends Component {
 
     async componentDidMount() {
         this.clickTableUpdateUser();
+        this.alertHoverTable();
     }
 
     clickTableUpdateUser = () => {
         $(document).on('click', '#usersdatatable table tbody tr td', function () {
             let valores = "";
-    
+
             // Obtenemos la primer columna de la fila seleccionada
             // seleccionada
             valores = $(this).parents("tr").find("td:first").html();
-    
+
             alert(valores);
-    
+
         });
+    }
+
+    alertHoverTable = () => {
+        $(document).ready(function(){
+            $('tr').not(':first').mouseover(function(){
+                var user = $(this).find('td:first-child').text();
+                $("#message-mouseover").empty();
+                $("#message-mouseover").append("Click sobre la fila para actualizar");  
+            })
+            .mouseout(function () {
+                $("#message-mouseover").empty();
+            });
+        });
+        /*$("#usersdatatable table tr")
+            .mouseover(function () {
+                let user  = $(this).parents("tr").find("td:first").html();
+                
+            })
+            .mouseout(function () {
+                $("#message-mouseover").empty();
+                $("#message-mouseover").append("Para actualizar, seleccione un usuario");  
+            });*/
+
     }
 
     render() {
         return (
             <div className="container">
-               <h2>Usuarios de la aplicación</h2>
+                <h2>Usuarios de la aplicación</h2>
                 <hr />
-                <div id="usersdatatable">
-                <UserDatatablePage />
+                <span id="message-mouseover"></span>
+                <div className="tableuser" id="usersdatatable">
+                    <UserDatatablePage />
                 </div>
             </div>
         )
