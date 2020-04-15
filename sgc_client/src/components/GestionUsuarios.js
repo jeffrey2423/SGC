89d424eafd1d7069d417dcea8cbb9cb1487bf28b
jpcv222 <juan.castro.vasquez@correounivalle.edu.co>
2020-04-15 01:50:14 -0500

    Add event to update user

--- sgc_client/src/components/GestionUsuarios.js
+++ sgc_client/src/components/GestionUsuarios.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react'
 import UserDatatablePage from './UserDatatablePage'
+import $ from 'jquery';
 
 export default class GestionUsuarios extends Component {
+
+    async componentDidMount() {
+        this.clickTableUpdateUser();
+    }
+
+    clickTableUpdateUser = () => {
+        $(document).on('click', '#usersdatatable table tbody tr td', function () {
+            let valores = "";
+    
+            // Obtenemos la primer columna de la fila seleccionada
+            // seleccionada
+            valores = $(this).parents("tr").find("td:first").html();
+    
+            alert(valores);
+    
+        });
+    }
+
     render() {
         return (
             <div className="container">
                <h2>Usuarios de la aplicación</h2>
                 <hr />
+                <div id="usersdatatable">
                 <UserDatatablePage />
+                </div>
             </div>
         )
     }
