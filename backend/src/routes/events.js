ad9a8e7ea12bb92c852252e9a7d18c97971dd959
Jeffrey Rios Muñoz <jeffrey.rios@siesa.com>
2020-04-18 21:36:03 -0500

    ultimas rutas para la gestion de eventos agregadas

--- backend/src/routes/events.js
+++ backend/src/routes/events.js
@@ -7,4 +7,20 @@ router.route('/getEvents/:id')
     //obtener datos
     .get(eventController.getEvents)
 
+router.route('/getEventsFilter')
+    //obtener datos con filtro
+    .get(eventController.getEventsFilter)
+
+router.route('/createEvent')
+    //obtener datos con filtro
+    .post(eventController.createEvent)
+
+router.route('/deleteEvent/:id')
+    //obtener datos con filtro
+    .put(eventController.deleteEvent)
+
+router.route('/updateEvent/:id')
+    //obtener datos con filtro
+    .put(eventController.updateEvent)
+
 module.exports = router;
