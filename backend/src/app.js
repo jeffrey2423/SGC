55350095cd9af434411c1975f0000c0f67b15e63
Jeffrey Rios Muñoz <jeffrey.rios@siesa.com>
2020-05-22 08:16:35 -0500

    cambios en servidor

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,27 +2,39 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 require('dotenv').config()
-const app = express();
 const authToken = require("./middlewares/auth_token");
 const config = require('./config/config')
+const bodyParser = require('body-parser');
+const app = express();
 
-//SETTINGS
-app.set('port', config.API_PORT ||  4000);
-
+app.use(morgan('dev'));
+app.use(function (req, res, next) { req.headers['if-none-match'] = 'no-match-for-this'; next(); });
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
-});
-app.use(express.json());
 
+    next();
+
+
+});
+app.options("/*", function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.sendStatus(200);
+});
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.use(authToken);
 app.use('/api/user', require('./routes/user'));
 app.use('/api/user/auth', require('./routes/auth'));
 app.use('/api/user/profile', require('./routes/profile'));
 app.use('/api/user/events', require('./routes/events'));
+
+//SETTINGS
+app.set('port', config.API_PORT || 4000);
 
 module.exports = app;
