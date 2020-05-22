f52ee7599a5cf6d11d40dbe96fa2f22675326860
Jeffrey Rios Muñoz <jeffrey.rios@siesa.com>
2020-05-22 18:26:12 -0500

    pruebas en server

--- sgc_client/src/config/config.js
+++ sgc_client/src/config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    SECRET_KEY: process.env.SECRET_KEY || "sgc_api",
+    SECRET_KEY: "sgc_api",
     COLABORADOR: 1,
     ADMINISTRADOR: 2,
     BASE_URL: "http://159.89.80.82:4000/"
