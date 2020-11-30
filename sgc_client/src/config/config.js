f845ac59ef13f27db91c223bbf169499b68d8a43
jpcv222 <juan.castro.vasquez@correounivalle.edu.co>
2020-11-30 02:27:24 -0600

    Update config

--- sgc_client/src/config/config.js
+++ sgc_client/src/config/config.js
@@ -2,8 +2,8 @@ module.exports = {
     SECRET_KEY: "sgc_api",
     COLABORADOR: 1,
     ADMINISTRADOR: 2,
-    BASE_URL: "http://18.222.141.239:4000/",
-    // BASE_URL: "http://localhost:4000/" //localhost
+    //BASE_URL: "http://18.222.141.239:4000/",
+     BASE_URL: "http://localhost:4000/" //localhost
     // BASE_URL: "https://backend-97o7x4yyv.vercel.app/" 
 
 }
